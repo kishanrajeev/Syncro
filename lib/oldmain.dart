7c2f5507c18312e1524d16408ef564ff3fd29953
Kishan <kishanrj@outlook.com>
2023-05-07 14:58:53 -0400

    Added refresh button

--- lib/oldmain.dart
+++ lib/oldmain.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/cupertino.dart';
+/*
+
+leading: IconButton(
+          onPressed: () {
+            _scaffoldKey.currentState!.openDrawer();
+          },
+          icon: Icon(Icons.settings),
+        ),
+ */
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:indexed/indexed.dart';
+import 'drawer.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(
     MaterialApp(
       theme: ThemeData.dark(),
@@ -24,6 +34,8 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   final _webViewController = FlutterWebviewPlugin();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
 
   @override
   void initState() {
@@ -60,20 +72,49 @@ class _WebViewPageState extends State<WebViewPage> {
       statusBarColor: Colors.grey[900],
     ));
 
-    return WebviewScaffold(
-      url: widget.url,
-      withZoom: true,
-      withLocalStorage: true,
-      hidden: true,
-      initialChild: Center(child: CircularProgressIndicator()),
+    var isBig = MediaQuery
+        .of(context)
+        .size
+        .width > 600.0; // set width threshold
+
+    return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Center(child: Text('Text Sync')),
+
+        title: Center(child: Text('Syncro')),
         actions: [
           IconButton(
             icon: Icon(Icons.refresh),
             onPressed: () {
               _webViewController.reload();
             },
+          ),
+        ],
+      ),
+      drawer: isBig ? null : Drawer(
+        width: 250,
+        backgroundColor: Colors.grey[900],
+        child: DrawerContent(),
+      ),
+      body: Indexer( // use Indexer instead of Stack
+        children: [
+          Indexed( // use Indexed instead of Positioned
+            index: 1, // lower index for web view
+            child: WebviewScaffold(
+              url: widget.url,
+              withZoom: true,
+              withLocalStorage: true,
+              hidden: true,
+              initialChild: Center(child: CircularProgressIndicator()),
+            ),
+          ),
+          if (isBig) Indexed( // use Indexed instead of Positioned
+            index: 2, // higher index for drawer
+            child: Drawer(
+              child: DrawerContent(),
+              backgroundColor: Colors.grey[900],
+              width: 250,
+            ), // position the drawer on the left
           ),
         ],
       ),
